e1d308f03d09802dd8e519f72c79aeeb24e93b69
Belen Neira <belen.neira05@gmail.com>
2023-05-30 12:24:23 -0400

    Creando funcionalidades para obtener el total de los links, los links rotos y los unicos

--- carpeta-prueba/archivo-prueba.md
+++
@@ -1,2 +0,0 @@
-# Esto es una prueba
-[link de prueba](https://github.com/Laboratoria/DEV005-md-links)
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,18 +2,19 @@ const { default: expect } = require('expect');
 const { mdLinks } = require('../index.js');
 const { absolutePath, existsPath, readDirectory } = require('../functions.js');
 
+
 describe('mdLinks', () => {
 
   it('mdLinks debría ser una función', () => {
     expect(typeof mdLinks).toBe('function');
   });
-  // it('Debería devolver una promesa', () => {
-  //  expect(mdLinks('mdLinks.md')).toBeInstanceOf(Promise);
-  // });
-  // it('Debe rechazar la promesa cuando el path no existe', () => {
-  //   const path = '/carpeta-prueba/archivo-pruebassss.txt'
-  //   expect(mdLinks(path)).rejects.toThrowError('La ruta no existe');
-  // });
+  it('Debería devolver una promesa', () => {
+   expect(mdLinks('mdLinks.md')).toBeInstanceOf(Promise);
+  });
+  it('Debe rechazar la promesa cuando el path no existe', () => {
+    const path = '/carpeta-prueba/archivo-pruebassss.txt'
+    expect(mdLinks(path)).rejects.toThrowError('La ruta no existe');
+  });
   it('Debería devolver true si la ruta existe', () => {
     const path = 'C:/Users/belen/Desktop/Laboratoria/DEV005-md-links/carpeta-prueba';
     const result = existsPath(path);
